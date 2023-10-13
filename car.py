49e9cfa100e628cbcd818ab4740dd7b18c63cede
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2023-10-14 00:05:50 +0700

    finish task refactoring based on new class diagram

--- car.py
+++ car.py
@@ -1,10 +1,19 @@
 from abc import ABC, abstractmethod
+from battery.battery import Battery
+from engine.engine import Engine
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Serviceable(ABC):
 
     @abstractmethod
-    def needs_service(self):
+    def needs_service(self) -> bool:
         pass
+
+
+class Car(Serviceable):
+    def __init__(self, engine: Engine, battery: Battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self) -> bool:
+        self.engine.needs_service() or self.battery.needs_service()
---
+++ car_factory.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from car import Car
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class CarFactory(metaclass=SingletonMeta):
+    def create_calliope(self, current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
+        return Car(CapuletEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date))
+
+    def create_glissade(self, current_date: datetime, last_service_date: datetime, current_mileage: int,
+                        last_service_mileage: int) -> Car:
+        return Car(WilloughbyEngine(last_service_mileage, current_mileage), SpindlerBattery(last_service_date))
+
+    def create_palindrome(self, current_date: datetime, last_service_date: datetime, warning_light_on: bool) -> Car:
+        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date))
+
+    def create_rorschach(self, current_date: datetime, last_service_date: datetime, current_mileage: int,
+                        last_service_mileage: int) -> Car:
+        return Car(WilloughbyEngine(last_service_mileage, current_mileage), NubbinBattery(last_service_date))
+
+    def create_thovex(self, current_date: datetime, last_service_date: datetime, current_mileage: int,
+                         last_service_mileage: int) -> Car:
+        return Car(CapuletEngine(last_service_mileage, current_mileage), NubbinBattery(last_service_date))
